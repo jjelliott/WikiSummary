f7ce749e0572bf874d5b40c58729f0c90b52a743
jjelliott <jesseelliott@outlook.com>
2016-07-18 15:04:03 -0400

    Completion and javadoc

--- src/wikisummary/WikiSummary.java
+++ src/wikisummary/WikiSummary.java
@@ -2,8 +2,6 @@ package wikisummary;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -18,11 +16,15 @@ import org.jsoup.nodes.Element;
  */
 public class WikiSummary
 {
-
-    public static void main(String[] args) throws URISyntaxException
+    private WikiSummary(){
+        System.err.println("Not an object class, constructor should not be accessed!");
+    }
+    public static void main(String[] args)
     {
-       
+        //inital setup
+        String encoding = System.getProperty("file.encoding");
         String topic = "";
+        //read from arguments
         if (args.length > 0)
         {
             topic = args[0];
@@ -31,18 +33,25 @@ public class WikiSummary
                 topic = topic + "_" + args[i];
             }
         }
-        else
+        else //or read from console
         {
-            while(topic.trim().equals("")){
-            Scanner input = new Scanner(System.in, "UTF-8");
-            System.out.println("Please enter a topic:");
-            topic = input.nextLine();
+            while (topic.trim().equals(""))
+            {
+                Scanner input = new Scanner(System.in, encoding);
+                System.out.println("Please enter a topic:");
+                topic = input.nextLine();
             }
             
-            topic = topic.trim().replaceAll(" ", "_");
+        topic = topic.trim().replaceAll(" ", "_");
         }
-        
-        String path;
+        //call method
+        System.out.println(getWikiSummary(topic));
+
+    }
+    public static String getWikiSummary(String topic)
+    {
+        //create path from user input
+        String path = "";
         try
         {
             path = "http://en.wikipedia.org/wiki/" + URLEncoder.encode(topic, "UTF-8");
@@ -50,28 +59,26 @@ public class WikiSummary
         {
             Logger.getLogger(WikiSummary.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //URL uri = new URL();
+        //Get page using Jsoup
         Document html = null;
         try
         {
             html = Jsoup.connect(path).userAgent("Mozilla").get();
         } catch (IOException e)
         {
-            System.err.println("Unable to reach page\nIf using international characters, please call from command line\nEx. java -jar WikiSummary.jar [Topic]");
-            System.exit(0);
+            return "Unable to reach page\nIf using international characters on Windows, please call from command line\nEx. java -jar WikiSummary.jar [Topic]";
         }
-        
-
+        //Get first paragraph
         Element intro = html.select("div#mw-content-text > p").first();
+        //Display paragraph or error if disambiguation reached
         if (intro.text().contains("may refer to"))
         {
-            System.err.println("Reached disambiguation. Try to be more specific.");
+            return "Reached disambiguation. Try to be more specific.";
         }
         else
-        System.out.println(intro.text());
+        {
+            return intro.text();
+        }
+    }
 
-    }
-    static String encodeUrl(String urlToEncode) throws URISyntaxException {
-        return new URI(urlToEncode).toASCIIString();
-    }
 }
